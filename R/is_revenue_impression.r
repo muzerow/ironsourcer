3aa4c2afe99aed246affbc4a4117608ebdc8500c
Muzerow <muzerow6@gmail.com>
2021-10-06 15:51:59 +0300

    Get rid of stringr import

--- R/is_revenue_impression.r
+++ R/is_revenue_impression.r
@@ -1,7 +1,6 @@
 #' Get Impression Level Revenue Data from Ad Revenue Measurement API
 #' @description Get Impression Level Revenue Data from Ad Revenue Measurement API <https://developers.is.com/ironsource-mobile/air/ad-revenue-measurements>
 #' @importFrom httr add_headers content GET
-#' @importFrom stringr str_glue
 #'
 #' @param date Date of report in UTC timezone
 #' @param app_key Application key
@@ -10,8 +9,10 @@
 #' @export
 
 is_revenue_impression <- function(date, app_key, bearer_token) {
-  url <- str_glue("https://platform.ironsrc.com/partners/adRevenueMeasurements/v1?date={date}&appKey={app_key}")
-  download_link <- GET(url, add_headers("Authorization" = bearer_token))
+  download_link <- GET("https://platform.ironsrc.com/partners/adRevenueMeasurements/v1?",
+                       query = list(date = date,
+                                    appKey = app_key),
+                       add_headers("Authorization" = bearer_token))
 
   if (download_link$status_code != 200) {
     stop(paste0("Error code ", download_link$status_code, ": ", content(download_link)$error))
